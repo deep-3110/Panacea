71d40daba6b6a6c6900163c8c898a7233b2be056
Deep Shah <47827027+deep-3110@users.noreply.github.com>
2022-03-22 22:04:53 +0530

    Initial Commit

--- Code/Final Year Project Updated/drug-.py
+++ Code/Final Year Project Updated/drug-.py
@@ -1,1 +1,20 @@
-drug_df=pd.read_csv(r'test\dataset\drug-rec\grouped_drugRec.csv')
+
+
+
+import dropbox
+
+client = dropbox.client.DropboxClient('fecjcnqs73dhim4')
+print ('linked account: ', client.account_info())
+
+f = open('working-draft.txt', 'rb')
+response = client.put_file('/magnum-opus.txt', f)
+print('uploaded: ', response)
+
+folder_metadata = client.metadata('/')
+print('metadata: ', folder_metadata)
+
+f, metadata = client.get_file_and_metadata('/magnum-opus.txt')
+out = open('magnum-opus.txt', 'wb')
+out.write(f.read())
+out.close()
+print (metadata)
